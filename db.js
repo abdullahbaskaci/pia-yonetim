c3622b96ee28f590647abc533f616523c9a3dee7
Abdullah Başkacı <abdullahbaskaci@gmail.com>
2025-12-30 13:32:50 +0300

    db config update for render

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const { Pool } = require('pg');
+
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false } // Uzak sunucu bağlantısı için gerekli
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-module.exports = { query: (text, params) => pool.query(text, params) };
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+};
